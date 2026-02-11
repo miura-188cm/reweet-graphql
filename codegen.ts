896dfdf78e302634e673018409136956bf37a920
T.Miura <mtaiga0213@icloud.com>
2026-02-11 11:40:17 +0900

    update: plugin から preset に移行

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,13 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:3000/api/graphql",
-  documents: ["graphql/schema/*.{gql,graphql}"],
+  schema: "./graphql/schema/*.{gql,graphql}",
+  documents: ["./graphql/schema/*.{gql,graphql}"],
   ignoreNoDocuments: true,
   generates: {
-    "graphql/__generated__/types.ts": {
-      plugins: [
-        "typescript",
-        "typescript-operations",
-        "typescript-react-apollo",
-      ],
+    "graphql/__generated__/": {
+      preset: "client",
+      plugins: [],
     },
   },
 };
